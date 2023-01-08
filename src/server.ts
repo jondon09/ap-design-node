1731f86125f05bc786d5fdd919552a76d1685534
John Nkanu <jondon09@gmail.com>
2023-01-08 05:00:19 +0000

    update: updated the code added routes

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import router from './router'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    
+    console.log('Hello from express')
+    res.status(200)
+    res.json({message: 'Hello'})
+
+})
+
+app.use('/api', router)
+
+export default app 
