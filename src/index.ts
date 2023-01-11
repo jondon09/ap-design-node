936a1da64a9a46f832bdc09cf5a6242fa66b8dc7
John Nkanu <jondon09@gmail.com>
2023-01-11 17:46:45 +0000

    update: updated the src added auth and handler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import app from './server'
 
 app.listen(3001, () => {
-    console.log('hello on http://localhost:3001')
+    console.log('server listening on 3001 http://localhost:3001')
 })  
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import express from 'express'
 import router from './router'
+import morgan from 'morgan'
+import { protect } from './modules/auth'
+import { createNewUser, signin } from './handlers/user'
 
 const app = express()
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
     
@@ -11,6 +19,8 @@ app.get('/', (req, res) => {
 
 })
 
-app.use('/api', router)
+app.use('/api', protect, router)
+app.post('/user', createNewUser)
+app.post('/signin', signin)
 
 export default app 
